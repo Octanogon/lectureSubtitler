9ed0cdddf206c681194f4d0745489771e4af64aa
Harry <harrybest800@gmail.com>
2019-11-17 01:50:38 +0000

    Made subtitler work

--- subtitler.py
+++ subtitler.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-SUBTITLE_LINE = "Dialogue: 0,{0},{1},Default,,0,0,0,,{\fad(500,500)}{2}"
+SUBTITLE_LINE = "Dialogue: 0,{0},{1},Default,,0,0,0,,{{\\fad(500,500)}}{2}"
 
 class Video():
     
@@ -31,11 +31,13 @@ class Video():
         with open("fadeTemplate.ass", 'r') as f:
             template = f.readlines()
             
+        print(template)
         # Add our subtitles
         for subtitle in self.subtitles:
-            template.append(subtitle)
+            template.append(subtitle + "\n")
+            print(subtitle)
             
-            
+        print(template)
         with open("fade.ass", 'w') as f:
             f.writelines(template)
             
@@ -44,4 +46,4 @@ class Video():
         
         self.makeSubtitleFile()
         
-        subprocess.call("ffmpeg -i {0} -vf subtitles=fade.ass -c:a copy subtitled_{0}".format(self.videoPath), stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        subprocess.call("ffmpeg -y -i {0} -vf subtitles=fade.ass -c:a copy subtitled_{0}".format(self.path))#, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
